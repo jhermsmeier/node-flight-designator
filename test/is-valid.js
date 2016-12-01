72008bf6af0ed6be0c448e51c427a9031acebc2c
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-12-01 18:12:58 +0100

    Update lib: Add flightDesignator.isValid()

--- test/is-valid.js
+++ test/is-valid.js
@@ -1,28 +1,38 @@
 var Flight = require( '..' )
 var assert = require( 'assert' )
 
+var validFlights = [
+  'U24511',
+  'LH2054',
+  'AS33',
+  'KLM645',
+  'YYY9999A',
+  'AS33C',
+  'X11',
+  '23 1234',
+  '123',
+  'R6750',
+  'AB6430',
+  '4U050',
+  'LH180',
+]
+
+var invalidFlights = [
+  '1',
+  '23',
+  'X1',
+  'AS0',
+  'XY00001',
+  'ASDFGHGF',
+]
+
 describe( 'FlightDesignator', function() {
-  describe( '#isValid()', function() {
+  
+  describe( '.isValid()', function() {
     
     describe( 'valid', function() {
       
-      var flights = [
-        'U24511',
-        'LH2054',
-        'AS33',
-        'KLM645',
-        'YYY9999A',
-        'AS33C',
-        'X11',
-        '23 1234',
-        '123',
-        'R6750',
-        'AB6430',
-        '4U050',
-        'LH180',
-      ]
-      
-      flights.forEach( function( flight ) {
+      validFlights.forEach( function( flight ) {
         it( flight, function() {
           assert.strictEqual( Flight.isValid( flight ), true )
         })
@@ -32,16 +42,7 @@ describe( 'FlightDesignator', function() {
     
     describe( 'invalid', function() {
       
-      var flights = [
-        '1',
-        '23',
-        'X1',
-        'AS0',
-        'XY00001',
-        'ASDFGHGF',
-      ]
-      
-      flights.forEach( function( flight ) {
+      invalidFlights.forEach( function( flight ) {
         it( flight, function() {
           assert.strictEqual( Flight.isValid( flight ), false )
         })
@@ -50,4 +51,32 @@ describe( 'FlightDesignator', function() {
     })
     
   })
+  
+  describe( '#isValid()', function() {
+    
+    describe( 'valid', function() {
+      
+      validFlights.forEach( function( flight ) {
+        it( flight, function() {
+          assert.strictEqual( Flight.parse( flight ).isValid(), true )
+        })
+      })
+      
+    })
+    
+    describe( 'invalid', function() {
+      
+      invalidFlights.forEach( function( flight ) {
+        it( flight, function() {
+          var airline = flight.slice( 0, 2 )
+          var number = flight.slice( 2, 6 )
+          var suffix = flight.slice( 6 )
+          assert.strictEqual( new Flight( airline, number, suffix ).isValid(), false )
+        })
+      })
+      
+    })
+    
+  })
+  
 })
